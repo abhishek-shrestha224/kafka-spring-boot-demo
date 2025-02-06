c7f21a46179bc2fe6859e21bb9fbd2540dab3fc5
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-06 14:39:08 +0000

    created RestController for kafka topics

--- src/main/java/world/hello/kafkademo/MessageController.java
+++ src/main/java/world/hello/kafkademo/MessageController.java
@@ -1,6 +1,7 @@
 package world.hello.kafkademo;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/messages")
 public class MessageController {
-
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    @PostMapping
     public void publish(@RequestBody final String message) {
         kafkaTemplate.send("Dinosaurs", message);
     }
