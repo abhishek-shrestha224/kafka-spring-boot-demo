9db5de2c6d594f9cbc763c96d71a14137801d0c4
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-06 14:37:54 +0000

    deleted MessageRecord

--- src/main/java/world/hello/kafkademo/MessageController.java
+++ src/main/java/world/hello/kafkademo/MessageController.java
@@ -1,12 +1,17 @@
 package world.hello.kafkademo;
 
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/messages")
 public class MessageController {
-    public String publish(){
 
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void publish(@RequestBody final String message) {
+        kafkaTemplate.send("Dinosaurs", message);
     }
 }
--- src/main/java/world/hello/kafkademo/MessageRecord.java
+++
@@ -1,6 +0,0 @@
-package world.hello.kafkademo;
-
-public record MessageRecord(
-        String message
-) {
-}
