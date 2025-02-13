e83c1ab371d21dd08cde360fe1a492b2d527c95a
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-13 12:25:06 +0000

    ...

--- src/main/java/world/hello/kafkademo/config/KafkaConsumerConfig.java
+++ src/main/java/world/hello/kafkademo/config/KafkaConsumerConfig.java
@@ -1,8 +1,7 @@
 package world.hello.kafkademo.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,8 +22,8 @@ public class KafkaConsumerConfig {
     public Map<String, Object> kafkaConsumerConfig() {
         HashMap<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
     }
 
@@ -33,6 +32,7 @@ public class KafkaConsumerConfig {
         return new DefaultKafkaConsumerFactory<>(kafkaConsumerConfig());
     }
 
+    @Bean
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory(
             final ConsumerFactory<String, String> consumerFactory
     ) {
